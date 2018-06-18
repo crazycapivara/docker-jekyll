ba9eaffc58f48962b36e7ea461961bbcda544ffe
stkuethe <crazycapivara@gmail.com>
2018-06-18 07:57:26 +0100

    Update sinatra app

--- app/server.rb
+++ app/server.rb
@@ -2,22 +2,23 @@ require "sinatra"
 require "thin"
 
 set :server, :thin
-set :environment, :production
 set :port, 4000
 set :bind, "0.0.0.0"
+set :environment, :production
 
 get "/" do
 	content_type :json
-	@code = system("ls")
-	{ :status => "Hello World", :exitCode => $?.exitstatus, :code => @code }.to_json
+	@status = system("ls")
+	{ :message => "Hello World!", :exitCode => $?.exitstatus, :status => @status }.to_json
 end
 
 get "/api/v1/:id" do
 	content_type :json
-	if params['id'] == "123"
-		system("ls")
-		puts "fine"
+	@status = false
+	if params['id'] == "12345"
+		puts "Here we go!"
+		@status = system("/etc/periodic/_drafts/pullandbuild")
 	end
-	{ :status => "OK", :id => params['id'] }.to_json
+	{ :id => params['id'], :status => @status }.to_json
 end
 
